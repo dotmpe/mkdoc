1b82ebfdb71b183ed3d2bcc0a665b57248153ec1
B. van Berkum <dev@dotmpe.com>
2015-08-23 18:59:27 +0200

    Testing $PATH on travis

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -17,16 +17,18 @@ test -d || mkdir -vp $SRC_PREFIX
 test -d || mkdir -vp $PREFIX
 
 # Check for BATS shell test runner or install
-test -x "$(which bats)" && {
-  bats --version
-} || {
+test -x "$(which bats)" || {
+
   echo "Installing bats"
   pushd $SRC_PREFIX
   git clone https://github.com/sstephenson/bats.git
   cd bats
   ./install.sh $PREFIX
   popd
-  bats --version
 }
+
+export PATH=$PATH:$PREFIX/bin
+
+bats --version
 
 # Id: git-versioning/0.0.27-test install-dependencies.sh
