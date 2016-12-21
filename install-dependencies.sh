53b373501ea7fff19a63421c13710ffa78520a94
B. van Berkum <dev@dotmpe.com>
2016-12-21 14:49:02 +0100

    Deps

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,11 +5,23 @@ set -e
 test -z "$Build_Debug" || set -x
 
 test -z "$Build_Deps_Default_Paths" || {
-  test -n "$SRC_PREFIX" || SRC_PREFIX=$HOME/build
-  test -n "$PREFIX" || PREFIX=$HOME/.local
+
+  test -n "$SRC_PREFIX" || {
+    test -w /src/ \
+      && SRC_PREFIX=/src/ \
+      || SRC_PREFIX=$HOME/build
+  }
+
+  test -n "$PREFIX" || {
+    test -w /usr/local/ \
+      && PREFIX=/usr/local/ \
+      || PREFIX=$HOME/.local
+  }
 }
 
 test -n "$sudo" || sudo=
+test -z "$sudo" || pref="sudo $pref"
+
 
 test -n "$SRC_PREFIX" || {
   echo "Not sure where checkout"
@@ -29,12 +41,33 @@ install_bats()
 {
   echo "Installing bats"
   local pwd=$(pwd)
+  test -n "$BATS_BRANCH" || BATS_BRANCH=master
   mkdir -vp $SRC_PREFIX
   cd $SRC_PREFIX
-  git clone https://github.com/dotmpe/bats.git
+  test -n "$BATS_REPO" || BATS_REPO=https://github.com/dotmpe/bats.git
+  test -n "$BATS_BRANCH" || BATS_BRANCH=master
+  test -d bats || {
+    git clone $BATS_REPO bats || return $?
+  }
   cd bats
-  ${sudo} ./install.sh $PREFIX
+  git checkout $BATS_BRANCH
+  ${pref} ./install.sh $PREFIX
   cd $pwd
+}
+
+
+install_mkdoc()
+{
+  echo "Installing mkdoc"
+  pushd $SRC_PREFIX
+  git clone https://github.com/dotmpe/mkdoc.git
+  cd mkdoc
+  git checkout devel
+  PREFIX=~/usr/ ./configure && ./install.sh
+  popd
+  rm Makefile
+  ln -s ~/usr/share/mkdoc/Mkdoc-full.mk Makefile
+  #make
 }
 
 
@@ -53,13 +86,17 @@ main_entry()
       PATH=$PATH:$PREFIX/bin bats --version
     ;; esac
 
+  case "$1" in -|mkdoc)
+      install_mkdoc || return $?
+    ;; esac
+
   echo "OK. All pre-requisites for '$1' checked"
 }
 
 test "$(basename $0)" = "install-dependencies.sh" && {
   while test -n "$1"
   do
-    main_entry $1 || exit $?
+    main_entry "$1" || exit $?
     shift
   done
 } || printf ""
