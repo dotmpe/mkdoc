4c5e9b0010b6f946d1148fdfc7724c20ff6b8c31
B. van Berkum <dev@dotmpe.com>
2017-02-05 00:01:36 +0100

    Install target

--- usr/share/mkdoc/Core/Rules.default.mk
+++ usr/share/mkdoc/Core/Rules.default.mk
@@ -277,8 +277,8 @@ cleandep::
 	 if test $$? -gt 0; then echo; fi; # put extra line if err-msg
 	@$(ll) OK $@ "$(call count,$(DEP) $(DMK)) dependencies removed"
 
-#install: test
-#	@$(echo) -e " $(mk_ok)  $(c2)nothing to install.$(c0)"
+install: $(INSTALL)
+	@$(echo) -e " $(mk_ok)  $(c2)nothing to install.$(c0)"
 
 all:: build test install
 	@$(ll) Done $@ 'built, tested and installed'
